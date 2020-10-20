83fb2fb6e6678cca232035456c8fabaf956489df
elnura15ae <71874115+elnura15ae@users.noreply.github.com>
2020-10-20 14:20:06 +0200

    updated

---
+++ JAVASCRIPT/tutorial1/app.js
@@ -0,0 +1,79 @@
+document.writ
+
+function hideit {
+    return intecbrusseldiv.innerHTML=5+5;
+}
+
+var test='hsqhsvshvlqshqqhdsivhslvsh'
+var test='hdshchcsdhcscsqqqqqqqqqqqq'
+// vartest='test'
+
+var intecbrussel='this is a string!';
+intecbrussel=123456789;
+var intecbrussel=true;
+intecbrussel= false;
+intecbrussel={}
+intecbrussel=[]
+intecbrussel=test;
+
+var x =1;
+var y =2;
+var z= x+y;
+console.log (intecbrussel)
+
+var myVar=function () { return 1};
+
+console.log (typeof myVar)
+
+/* JavaScript Operators
+arithmetic operators
+( +-*/
+
+var num1=5;
+var num2= 9;
+var result=num1+num2;
+result=num1-num2;
+result=num1*num2;
+result=num1/num2;
+
+var str1="Intec"
+var str2="Brussel"
+
+result=str1+str2;
+
+num1= "5";
+num2="9";
+console.log (result)
+
+var user, pass, email, verify, mobile;
+ user='root'
+ pass=121212122;
+ email="hflqhv@sjdlfmvkd.com";
+ verify=true;
+ mobile="+48515446445852"
+
+ password = 121212122;
+
+ console.log(pass===password)
+
+
+const test1= "ths is const variable";
+//test1:"agdgdgddhdh"
+//const test1="bccnckkkhscqsvkr"
+//console.log(test1)
+
+
+let test2="this is test let variable";
+test2="test has been updated."// re-update
+test2=12466468465451 //re-decleration
+console.log(test2)
+
+
+// comment single single
+
+/*
+hfksvhs4
+jvlvvj
+*/
+
+
