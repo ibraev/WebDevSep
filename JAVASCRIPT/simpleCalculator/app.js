6081355ca314090769f9126720c365ca334b6d1d
elnura15ae <71874115+elnura15ae@users.noreply.github.com>
2020-10-26 18:31:07 +0100

    updated

--- JAVASCRIPT/simpleCalculator/app.js
+++ JAVASCRIPT/simpleCalculator/app.js
@@ -13,3 +13,17 @@ function submit1() {
     
     return result.innerText= `Result> ${Number(num1.value) + Number(num2.value)}`;
 }
+
+//define a function to find biggest number!
+//findBiggestNumber(10,20);
+//expected biggest number is 20.
+
+function findBiggestNummer (a,b) {
+  if (a < b) {
+      return `${b} buyuktur ${a}'dan`
+  }
+    return `${b} kucuktur ${a}'dan`
+}
+ console.log(findBiggestNummer(100,50));
+
+
