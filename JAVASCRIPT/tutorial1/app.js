384288deb9fc07fa3ecd8e994c258edc51f37ced
elnura15ae <71874115+elnura15ae@users.noreply.github.com>
2020-10-27 13:59:25 +0100

    updated

--- JAVASCRIPT/tutorial1/app.js
+++ JAVASCRIPT/tutorial1/app.js
@@ -1,97 +1,97 @@
-document.write ('hello world!')
-// alert (5)
+document.write('hello world!');
+// alert(5)
 
-var intecbrussel=document.getElementById('intecbrussel');
+var intecbrusseldiv=document.getElementById('intecbrussel');
 
-intecbrussel.innerHTML="<div class='row'>Blabla</div>";
+intecbrusseldiv.innerHTML="<div class='row'>Blabla</div>";
+
+console.log('Test console log')
+
 
 function hideit(){
-    return intecbrussel.style.display="none";
-}
-
-//alert {This is alert!}
-
-//console.log(); i see all the data to use, to analyse the data
-
-//fetch 'https://jsonplaceholder.typ1code.com/todos/1';
-//.then (res=>res)
-//.then (data=>)
-// console.log(data)
-
-function hideit {
     return intecbrusseldiv.innerHTML=5+5;
 }
 
-var test='hsqhsvshvlqshqqhdsivhslvsh'
-var test='hdshchcsdhcscsqqqqqqqqqqqq'
-// vartest='test'
+var test='asjdiaojdioajsdiojasiodjaiosdjsioajsdioaj';
+var test = 'asidijasidjioasjdiojasdoijasiodjaiod';
+// vartest='test';
 
-var intecbrussel='this is a string!';
-intecbrussel=123456789;
-var intecbrussel=true;
-intecbrussel= false;
-intecbrussel={}
-intecbrussel=[]
-intecbrussel=test;
+var intecbrussel = 'this is a string';
+intecbrussel = 12345667788;
+var intecbrussel = true;
+intecbrussel = false;
+intecbrussel= {}
+intecbrussel=[];
+intecbrussel= test;
 
-var x =1;
-var y =2;
-var z= x+y;
-console.log (intecbrussel)
+var x = 1;
+var y = 2;
+var z = x+y;
+console.log(z)
 
-var myVar=function () { return 1};
+console.log(intecbrussel)
 
-console.log (typeof myVar)
 
-/* JavaScript Operators
+var myVar=function(){ return 1 };
+
+console.log(typeof myVar)
+
+
+/*
+JavaScript Operators
 arithmetic operators
-( +-*/
+( + - * / )
+*/
 
-var num1=5;
-var num2= 9;
-var result=num1+num2;
-result=num1-num2;
-result=num1*num2;
-result=num1/num2;
-
-var str1="Intec"
-var str2="Brussel"
-
-result=str1+str2;
-
-num1= "5";
-num2="9";
-console.log (result)
-
-var user, pass, email, verify, mobile;
- user='root'
- pass=121212122;
- email="hflqhv@sjdlfmvkd.com";
- verify=true;
- mobile="+48515446445852"
-
- password = 121212122;
-
- console.log(pass===password)
+var num1 = 5;
+var num2 = 9;
+var result = num1+num2;
+result = num1 - num2;
+result = num1 * num2;
+result = num1 / num2;
 
 
-const test1= "ths is const variable";
-//test1:"agdgdgddhdh"
-//const test1="bccnckkkhscqsvkr"
-//console.log(test1)
+var str1 = "Intec "
+var str2 = "Brussel"
+
+result = str1 + str2;
 
 
-let test2="this is test let variable";
-test2="test has been updated."// re-update
-test2=12466468465451 //re-decleration
-console.log(test2)
+num1 = "5";
+num2 = "9";
+console.log(result)
 
+
+var user,pass,email,verify,mobile;
+
+user= 'root'
+pass= 12121212;
+email ="asdasd@asdsad.com";
+verify=true;
+mobile="+4484484848484";
+
+password = 12121212;
+
+
+console.log(pass == password)
 
 // comment single single
 
 /*
-hfksvhs4
-jvlvvj
+kasdkoaskd
+asdasdasd
+sadasdasdasd
+asdasdasd
 */
 
+
+const test1 = "this is const variable";
+// test1="asdasdasd";
+// const test1= "teadasdadadadas"
+// console.log(test1)
+
+let test2 = "this is let variable";
+test2="let has been updated." // re-update
+let test2 = 372897372347239483 // re-decleration
+console.log(test2)
 
