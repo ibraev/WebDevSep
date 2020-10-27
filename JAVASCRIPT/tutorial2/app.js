7e73c385c378961b6628497531209a8d2ff57132
elnura15ae <71874115+elnura15ae@users.noreply.github.com>
2020-10-27 14:06:17 +0100

    updated

--- JAVASCRIPT/tutorial2/app.js
+++ JAVASCRIPT/tutorial2/app.js
@@ -1,54 +1,70 @@
-// JAVASCRIPT-VARIABLES
+// JAVASCRIPT - VARIABLES
 
 /*
-VAR variable
+VAR variable 
     re-update
     re-declare
 */
+
 var intecbrussel=1;
-var intecbrussel="This is string"
-var intecbrussel=true;
+intecbrussel = "this is string"
+var intecbrussel = true;
 
-console.log(intecbrussel)
 
-/* _$ 5  it is not recommended but its working*/
 
-var $intecbrussel="this is special char"
-console.log($intecbrussel)
-//to use special charecteristics is not recommended.
+/* _ $ 5 */ 
 
-intecbrussel ={
-    title:"this is title"
+var 义 = "This is special char"
+
+console.log(义)
+
+// to use special chars is not recommended.
+
+
+intecbrussel = {
+    title:"This is title"
 }
 
-intecbrussel=[1,2,3,4,5,6,7,8,9]
+intecbrussel = [1,2,3,4,5,6,7];
 
 console.log(intecbrussel)
 console.log(typeof intecbrussel)
+
 
 // another variable
 
 var test1;
 test1=intecbrussel
 
-test=function myfunction(){return 1}
+test1 = function myfunc(){return 1}
 console.log(typeof test1)
 
-/* let variable
-    re-update*
+
+
+// var while = 1;
+
+// console.log(while)
+
+
+/*
+let variable
+    re-update *
 */
+
 let test = 5;
 test=9;
-// let test="this is new value"-not working
+// let test = "this is new value"
 console.log(test)
 
-//let only re-update
-//var re-update and re-declare
-//const
 
-//const variable - can not update
+// let only re-update
+// var - re-update and re-declare
+// const - read-only / It's static variable
 
-const NewVariable="This is static variable"
-//NewVariable="update it!"
-//const NewVariable="This is not static?"
+
+// const variable
+
+const NewVariable= 'This is static variable'
+// NewVariable = "update it!"
+// const NewVariable = "This is not static?"
 console.log(NewVariable)
