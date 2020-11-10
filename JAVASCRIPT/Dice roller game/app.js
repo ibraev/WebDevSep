40fec651e62bfa794f882a272fb9b40566b80fb2
elnura15ae <71874115+elnura15ae@users.noreply.github.com>
2020-11-10 14:58:20 +0100

    updated

--- JAVASCRIPT/Dice roller game/app.js
+++ JAVASCRIPT/Dice roller game/app.js
@@ -8,8 +8,8 @@ let textPlayer = document.getElementById('textPlayer');
 function changeDice() {
     const firstRandomDice = Math.floor(Math.random() * 6) + 1;
     const secondRandomDice = Math.floor(Math.random() *6) +1;
-    diceOne.innerHTML =`<img src="assets/dice${firstRandomDice}.png" style="he">`;
-    diceTwo.innerHTML = `<img src= "assets/dice${secondRandomDice}.png" style="hello">`;
+    diceOne.innerHTML =`<img src="assets/dice${firstRandomDice}.png">`;
+    diceTwo.innerHTML = `<img src= "assets/dice${secondRandomDice}.png">`;
     if (firstRandomDice > secondRandomDice) {
         textPlayer.innerHTML = `First Player Wins`;
     } else {
