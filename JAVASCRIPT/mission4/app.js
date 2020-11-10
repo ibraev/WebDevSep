c3257a4359e66f055e1272c6ffa6ad816365679e
elnura15ae <71874115+elnura15ae@users.noreply.github.com>
2020-11-10 16:01:35 +0100

    updated

--- JAVASCRIPT/mission4/app.js
+++ JAVASCRIPT/mission4/app.js
@@ -1,31 +1,42 @@
 var students = [
-
-    {​​
+    {
         name: "Student1",
         score: 10
-    }​​,
-    {​​
+    },
+    {
         name: "Student2",
         score: 8
-    }​​,
-    {​​
+    },
+ 
+    {
         name: "Student3",
         score: 5
-    }​​,
-    {​​
+    },
+    {
         name: "Student4",
         score: 3
-    }​​
+    }
 ]
-
-/*
-get student names who got 5 or >5 and print alphabetically sorted new list
-*/
-
-// use function
-
-CheckScore() // function
-
-getStudentName // function
-
-printerService // function
+ 
+function CheckScore(student) {
+    return student.score >= 5
+ 
+}
+ 
+function getStudentName(student) {
+ 
+    return student.name
+ 
+}
+ 
+function printerService(data) {
+ 
+    return data
+ 
+        .filter(CheckScore).sort((a, b) => { return a.score - b.score }).map(
+            getStudentName)
+ 
+} 
+ 
+let result = printerService(students);
+console.log(result)
