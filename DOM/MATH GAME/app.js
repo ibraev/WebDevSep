82117bf172ec402c2542a210bde20792432b0599
elnura15ae <71874115+elnura15ae@users.noreply.github.com>
2020-11-16 14:35:31 +0100

    updated

--- DOM/MATH GAME/app.js
+++ DOM/MATH GAME/app.js
@@ -12,13 +12,14 @@
 //  eval(number1 + operators[Math.round(Math.round()*3)] +number2)
 
  ////
-
+ 
  let n1= Math.floor(Math.random()*20+1);
- let n1= Math.floor(Math.random()*20+1);
+ let n2= Math.floor(Math.random()*20+1);
 
- document.getElementsById("intext").value = n1; // suroo1
- document.getElementsById("intext1").value = n2; // suroo1
+ document.getElementsById("firstnumber").value = n1; // suroo1
+ document.getElementsById("secondnumber").value = n2; // suroo2
 let adds = n1 + n2;
+
 function Game() {
     var user = document.getElementsById("answer".value);
     if (user == adds){
@@ -29,9 +30,35 @@ function Game() {
     var user = document.getElementsById("question".value) = "";
 
     n1= Math.floor(Math.random()*20+1);
-    n1= Math.floor(Math.random()*20+1);
+    n2= Math.floor(Math.random()*20+1);
 
- document.getElementsById("intext").value = n1; // suroo1
- document.getElementsById("intext1").value = n2;
+ document.getElementsById("firstnumber").value = n1; // suroo1
+ document.getElementsById("secondnumber").value = n2;
  adds = n1 + n2;
 }
+
+
+
+
+// let n1= Math.floor(Math.random()*20+1);
+//  let n2= Math.floor(Math.random()*20+1);
+
+//  document.getElementsById("intext").value = n1; // suroo1
+//  document.getElementsById("intext1").value = n2; // suroo2
+// let adds = n1 + n2;
+// function Game() {
+//     var user = document.getElementsById("answer".value);
+//     if (user == adds){
+//         document.getElementsById("question").innerHTML = "Well Done! Your Answer id Correct";
+//     } else {
+//         document.getElementsById("question").innerHTML = "Correct Answer" + adds + " . Try Again";
+//     }
+//     var user = document.getElementsById("question".value) = "";
+
+//     n1= Math.floor(Math.random()*20+1);
+//     n1= Math.floor(Math.random()*20+1);
+
+//  document.getElementsById("intext").value = n1; // suroo1
+//  document.getElementsById("intext1").value = n2;
+//  adds = n1 + n2;
+// }
