027739dc95474ffe928ecff581a446ae47a65655
elnura15ae <71874115+elnura15ae@users.noreply.github.com>
2020-11-16 09:02:53 +0100

    updated

--- JAVASCRIPT/javatest/app.js
+++ JAVASCRIPT/javatest/app.js
@@ -1,16 +1,16 @@
 
-let myImage = document.querySelector('img');
-myImage.onclick = function() {
-    let mySrc = myImage.getAttribute('src');
-    if (mySrc === 'images/memoire.png') {
-        myImage.setAttribute('src', 'images/calculmental.png')
-    } else {
-        myImage.setAttribute('src', 'images/memoire.png')
-    }
-}
+// let myImage = document.querySelector('img');
+// myImage.onclick = function() {
+//     let mySrc = myImage.getAttribute('src');
+//     if (mySrc === 'images/memoire.png') {
+//         myImage.setAttribute('src', 'images/calculmental.png')
+//     } else {
+//         myImage.setAttribute('src', 'images/memoire.png')
+//     }
+// }
 
 
-et myButton = document.querySelector('button');
+let myButton = document.querySelector('button');
 let myHeading = document.querySelector('h1');
 
 function setUserName() {
@@ -18,3 +18,13 @@ function setUserName() {
     localStorage.setItem('name', myName);
     myHeading.textContent = 'Mozilla is cool, ' + myName;
     }
+
+if(!localStorage.getItem('name')) {
+    setUserName();
+} else {
+    let storeName = localStorage.getItem('name');
+    myHeading.textContent = 'Mozilla is cool, ' + storeName;
+}
+myButton.onclick = function (){
+    setUserName();
+}
