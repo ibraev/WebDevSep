8805f7dabd94953ce30743b5ef7ce6effb9f852a
elnura15ae <71874115+elnura15ae@users.noreply.github.com>
2020-11-16 14:00:05 +0100

    updated

---
+++ DOM/MATH GAME/app.js
@@ -0,0 +1,37 @@
+
+// let operators = ["+","-","*","/"]
+// operators[Math.round(Math.random()*4)+1]
+// operators[Math.random(Math.random()*3)]
+//  var number1 = 5
+//  var number2 = 10
+// function mathgame(){
+
+// }
+//  number1+ operators[Math.round(Math.round())*3)] number2
+//  "5-10"
+//  eval(number1 + operators[Math.round(Math.round()*3)] +number2)
+
+ ////
+
+ let n1= Math.floor(Math.random()*20+1);
+ let n1= Math.floor(Math.random()*20+1);
+
+ document.getElementsById("intext").value = n1; // suroo1
+ document.getElementsById("intext1").value = n2; // suroo1
+let adds = n1 + n2;
+function Game() {
+    var user = document.getElementsById("answer".value);
+    if (user == adds){
+        document.getElementsById("question").innerHTML = "Well Done! Your Answer id Correct";
+    } else {
+        document.getElementsById("question").innerHTML = "Correct Answer" + adds + " . Try Again";
+    }
+    var user = document.getElementsById("question".value) = "";
+
+    n1= Math.floor(Math.random()*20+1);
+    n1= Math.floor(Math.random()*20+1);
+
+ document.getElementsById("intext").value = n1; // suroo1
+ document.getElementsById("intext1").value = n2;
+ adds = n1 + n2;
+}
--- DOM/tutorial2/js/app.js
+++ DOM/tutorial2/js/app.js
@@ -1,31 +1,30 @@
-// navigation in dom tree
+// nevigation in dom three
 
-var CardBody = document.querySelectorAll('.card-body')
-var listofTodos = document.querySelector('.list-group')
-var singleTodo = document.querySelector('.list-group-item:nth-child(5)')
-var nthChildsofTodos = document.querySelectorAll('.list-group-item:nth-child(even)')
+var listofTodos =document.querySelector('.list-group')
+var singleTodos = document.querySelector('.list-group-item:nth-child')
+var nthChildsofTodos = document.querySelectorAll('.list-group-item:nth-child(event')
 
-console.log(nthChildsofTodos)
+console.log(nthChildofTodos)
 console.log(listofTodos)
 
-console.log(singleTodo)
+console.log(singleTodos)
 
-
-nthChildsofTodos.forEach(function(todo){
-    todo.className="list-group-item d-flex justify-content-between bg-secondary"
+nthChildsofTodos.forEach(function (todo){
+    todo.className="list-group"//// not finished line
 })
 
-// child nodes
+// child nodels
 
 var ConsoleResult;
-ConsoleResult = listofTodos.childNodes[0];
-ConsoleResult = listofTodos.children[0];
-console.log("Console Result>",ConsoleResult)
+ConsoleResult = listofTodos.childNodes[0]
+ConsoleResult = listofTodos.childNodes[listofTodos.children.length-1]
+console.log("Console Result>", ConsoleResult)
 
-let arr  = [1,2,3,4,5,6,"this is last item"]
+let arr = [1,2,3,4,5,6,7]
+console.log(arr[2])
 console.log(arr[arr.length-1])
 
-ConsoleResult.innerHTML="This is todo title - new "
+ConsoleResult.innerHTML="This is todo title - new"
 
 ConsoleResult = CardBody[1];
 ConsoleResult = ConsoleResult.children[1].textContent="Title has been updated";
@@ -33,17 +32,20 @@ ConsoleResult = CardBody[1];
 ConsoleResult = ConsoleResult.firstElementChild
 ConsoleResult = CardBody[1];
 //ConsoleResult = ConsoleResult.childElementCount;
-//ConsoleResult = ConsoleResult.children.length;
-//ConsoleResult = ConsoleResult.lastElementChild
+// ConsoleResult = ConsoleResult.children.length;
+// ConsoleResult = ConsoleResult.LastElementChild;
 
+console.log(ConsoleResult)
 
-// nextElementSibling 
+// nextElementSibling
 
-//ConsoleResult = singleTodo.nextElementSibling;
+// ConsoleResult = singleTodo.nextElementSibling;
 
 // previousElement
+
 //ConsoleResult = singleTodo.previousElementSibling
-ConsoleResult = singleTodo.nextElementSibling.nextElementSibling
+
+ConsoleResult= singleTodo.nextElementSibling.nextElementSibling
 ConsoleResult = singleTodo.previousElementSibling.previousElementSibling
 
 console.log("Result", ConsoleResult)
