090a1c9f4f04c5555477ce95d86f2d8daa455887
elnura15ae <71874115+elnura15ae@users.noreply.github.com>
2020-11-21 14:41:29 +0100

    updated

--- JAVASCRIPT/8 Must Know JavaScript Array Methods/app.js
+++ JAVASCRIPT/8 Must Know JavaScript Array Methods/app.js
@@ -22,14 +22,46 @@ console.log (itemNames);
 
 // egerde oshonu ele price ka almashtyrsak baasy chygat
 
-const itemNames = items.map((item) => { 
-    return item.price  ///  baalary chykty
-})
-console.log (itemNames);
+const itemPrice = items.map((item) => { 
+     return item.price  ///  baalary chykty
+ })
+ console.log (itemPrice);
 
-// find() misali kitap barby oshonu izdeybiz
+// find() misali kitap barby oshonu izdeybiz, we will find single object
 
-const itemNames = items.map((item) => { 
-    return item.price  ///  baalary chykty
+ const foundItem = items.find((item) => { 
+     return item.name === 'Book'  ///  baalary chykty
+ })
+ console.log (foundItem);
+
+// forEach very easy to use for every single element
+
+items.forEach((item) => {
+    console.log(item.price)
 })
-console.log (itemNames);
+
+// baasy 100 don ashkan barby je jokbu? oshonu teksherebiz
+const hasInexpensiveItems = items.some((item) => {
+    return item.price <= 100
+})
+console.log(hasInexpensiveItems)
+
+// every checks every single item
+const checkseveryitem = items.every((item) => {
+    return item.price <= 100 //false
+})
+console.log(checkseveryitem) // false it checks entire products
+
+//  reduce () method calculates total price of products
+
+const total = items.reduce((currentTotal, item) => {
+    return item.price + currentTotal
+},0)
+console.log(total) // 2822
+
+/////// INCLUDES METHOD
+
+// const items = [1,2,3,4,5]
+// const includesTwo= items.includes(2) // true , we have item 2, if we put 7 is false, bcs we dont have
+
+// console.log(includesTwo)
